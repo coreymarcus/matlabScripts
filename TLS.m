b8fb43498d76a387fa4a1f34f5731cf8b98ca2a2
coreymarcus <corey.marcus@gmail.com>
2020-12-17 16:11:10 -0600

    Create TLS.m

---
+++ TLS.m
@@ -0,0 +1,42 @@
+function [m, b] = TLS(x, y)
+% TLS - Total Least Squares. Performs a single dimension linear fit of a
+% line y=mx+b to a set of x and y data points. Use when there is
+% measurement noise in x and y, and when you are equally confident in all
+% measurements. Sourced from "A tutorial on the total least squares method
+% for fitting a straight line and a plane" by Munoz et al, 2014
+% 
+% INPUTS:
+% x - an [nx1] or [1xn] vector of x measurements
+% y - an [nx1] or [1xn] vector of y measurements
+% 
+% OUTPUTS:
+% m - slope of best fit line
+% b - y intercept of best fit line
+
+%number of data points
+n = length(x);
+
+%average x and y
+x_tilde = sum(x)/n;
+y_tilde = sum(y)/n;
+
+%calculate two sums
+sum_num = 0;
+sum_den = 0;
+for ii = 1:n
+    sum_num = sum_num + (x(ii) - x_tilde)*(y(ii)-y_tilde);
+    sum_den = sum_den + (y(ii) - y_tilde)^2 - (x(ii) - x_tilde)^2;
+end
+
+%find phi
+phi = 0.5*atan2(-2*sum_num,sum_den);
+
+%find r
+r = x_tilde*cos(phi) + y_tilde*sin(phi);
+
+%find line parameters
+m = -1/tan(phi);
+b = r/sin(phi);
+
+end
+
