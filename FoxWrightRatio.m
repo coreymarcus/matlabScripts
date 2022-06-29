9bcfeddfb9a08556f8dd1a5b24c4e782b9b343e9
coreymarcus <corey.marcus@gmail.com>
2022-06-29 11:30:40 -0500

    added support for non-integer alpha

--- FoxWrightRatio.m
+++ FoxWrightRatio.m
@@ -12,7 +12,9 @@ if(alpha == 1)
     end
 
     R = x/2 + (1/(2*sqrt(pi))) * exp(-(x^2)/4) / (1-Phi);
-
+elseif( alpha < 1)
+    % In this case we are forced to evaluate the ratio directly
+    R = FoxWright((alpha + 1)/2,x,250)/FoxWright(alpha/2,x,250);
 else
     % Run a recursion
     R = x/2 + (alpha-1)/2*(1/FoxWrightRatio(alpha - 1,x));
